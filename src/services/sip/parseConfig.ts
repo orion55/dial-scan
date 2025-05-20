0854c3693eab7b94aaf3ba79f267e433c0a88a04
grebenev.o.e <grebenev.o.e@atmsoft.ru>
2025-05-20 11:33:13 +0500

    Add parseConfig

---
+++ src/services/sip/parseConfig.ts
@@ -0,0 +1,45 @@
+export const parseConfig = (
+  configText: string,
+): Map<string, Record<string, string>> => {
+  const lines = configText.split(/\r?\n/);
+  const sections = new Map<string, Record<string, string>>();
+
+  let currentKey: string | null = null;
+  let currentObj: Record<string, string> = {};
+
+  const headerRegex = /^\s*(\[[^\]]+\](?:\([^)]+\))?)\s*$/;
+
+  for (const rawLine of lines) {
+    const line = rawLine.trim();
+
+    if (!line || line.startsWith(";") || line.startsWith("#")) {
+      // Пропускаем пустые строки и комментарии
+      continue;
+    }
+
+    const headerMatch = line.match(headerRegex);
+    if (headerMatch) {
+      // Новая секция
+      if (currentKey) {
+        // Сохраняем предыдущую
+        sections.set(currentKey, currentObj);
+      }
+      currentKey = headerMatch[1];
+      currentObj = {};
+    } else if (currentKey) {
+      // Параметр внутри текущей секции
+      const idx = line.indexOf("=");
+      if (idx > 0) {
+        const name = line.substring(0, idx).trim();
+        currentObj[name] = line.substring(idx + 1).trim();
+      }
+    }
+  }
+
+  // Добавляем последнюю секцию
+  if (currentKey) {
+    sections.set(currentKey, currentObj);
+  }
+
+  return sections;
+};
--- src/services/sip/sip.service.ts
+++ src/services/sip/sip.service.ts
@@ -3,21 +3,24 @@ import { readFile, writeFile } from "node:fs/promises";
 import path from "path";
 import { SettingsDial } from "../types/settings.types";
 import { removeComments } from "../helpers/removeComments";
+import { parseConfig } from "./parseConfig";
+import { logger } from "../logger.service";
 
 export const loadSip = async (
   filename: string,
   settings: SettingsDial,
 ): Promise<unknown> => {
-  const sipTrunks: SipTrunkMap<"context"> = new Map();
-  const sipUsers: SipUserMap<"parent" | "context"> = new Map();
-
+  logger.info(`Читаем файл ${filename}`);
   const filePath = path.join(settings.asterisk_config, filename);
   let text = await readFile(filePath, {
     encoding: "utf-8",
   });
 
   const cleanedText = removeComments(text);
-  // await writeFile("d:\\config-clean.conf", cleanedText, "utf-8");
+  const parsedText = parseConfig(cleanedText);
+
+  const sipTrunks: SipTrunkMap<"context"> = new Map();
+  const sipUsers: SipUserMap<"parent" | "context"> = new Map();
 
   return undefined;
 };
